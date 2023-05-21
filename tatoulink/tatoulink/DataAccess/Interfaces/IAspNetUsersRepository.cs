37b057ebb59f92085fe7d4718b5961bd975f57ec
Lenoic CRAMON <lenoic.cramon@epita.fr>
2023-05-21 08:11:38 +0200

    cleanup du code

--- tatoulink/tatoulink/DataAccess/Interfaces/IAspNetUsersRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using tatoulink.DataAccess.EfModels;
-
-namespace tatoulink.DataAccess.Interfaces
-{
-    public interface IAspNetUsersRepository
-    {
-        public Task<IEnumerable<Dbo.AspNetUsers>> Get(string includeTables = "");
-    }
-}
